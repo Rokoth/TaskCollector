40c8166a4435f1a131b21cf55055ff9fceee63c7
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2021-03-31 09:02:20 +0500

    Init repo

---
+++ App/Server/TaskCollector/TaskCollector/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TaskCollector.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
