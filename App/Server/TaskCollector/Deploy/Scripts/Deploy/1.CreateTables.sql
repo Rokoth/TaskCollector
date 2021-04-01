652b8281b7f5a090da172680f881d969e6e547fa
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2021-04-01 09:42:39 +0500

    Add table user

---
+++ App/Server/TaskCollector/Deploy/Scripts/Deploy/1.CreateTables.sql
@@ -0,0 +1,12 @@
+﻿create extension if not exists "uuid-ossp";
+
+create table if not exists "user"(
+      id            uuid          not null default uuid_generate_v4() primary key
+	, "name"        varchar(100)  not null
+	, "description" varchar(1000) not null
+	, "login"       varchar(100)  not null
+	, "password"    varchar(1000) not null
+	, version_date  timestamptz   not null default now()
+	, is_deleted    boolean       not null
+);
+
