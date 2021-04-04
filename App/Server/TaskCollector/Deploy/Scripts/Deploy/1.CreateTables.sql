22718df4cab3da5d094a278712937a18560445cc
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2021-04-04 14:22:09 +0500

    - techtask - client table

--- App/Server/TaskCollector/Deploy/Scripts/Deploy/1.CreateTables.sql
+++ App/Server/TaskCollector/Deploy/Scripts/Deploy/1.CreateTables.sql
@@ -10,3 +10,14 @@ create table if not exists "user"(
 	, is_deleted    boolean       not null
 );
 
+create table if not exists client(
+	  id            uuid          not null primary key
+	, "name"        varchar(100)  not null
+	, "description" varchar(1000) null
+	, "login"       varchar(100)  not null
+	, "password"    varchar(100)  not null
+	, map_rules     jsonb         null
+	, user_id       uuid          not null
+	, version_date  timestamptz   not null
+	, is_deleted    boolean       not null default false	
+);
