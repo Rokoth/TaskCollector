3b1bf3a60a7c3b8a7dcc1b50263b708e3b5ecb16
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2021-04-05 09:12:14 +0500

    Add message indexes

--- App/Server/TaskCollector/Deploy/Scripts/Deploy/3.CreateIndexes.sql
+++ App/Server/TaskCollector/Deploy/Scripts/Deploy/3.CreateIndexes.sql
@@ -14,3 +14,16 @@ create index idx_client_name
 
 create index idx_client_user_id
     on client("user_id");
+
+--message
+create index idx_message_level
+    on "message"("level");
+
+create index idx_message_title
+    on "message"("title");
+
+create index idx_message_client_id
+    on "message"(client_id);
+
+create index idx_message_created_date
+    on "message"(created_date);
