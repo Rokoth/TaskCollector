cb1354452af6bced33b33d0883e711991c1b42f7
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2021-04-11 21:49:09 +0500

    foreign keys

--- App/Server/TaskCollector/Deploy/Scripts/Deploy/2.CreateFK.sql
+++ App/Server/TaskCollector/Deploy/Scripts/Deploy/2.CreateFK.sql
@@ -14,9 +14,18 @@ add constraint fk_message_client_id
 		on delete no action on update no action;
 
 
+--message_status
+alter table message_status 
+add constraint fk_message_status_user_id 
+	foreign key("user_id") 
+		references "user"(id) 
+		on delete no action on update no action;
 
-
-
+alter table message_status 
+add constraint fk_message_status_message_id 
+	foreign key(message_id) 
+		references "message"(id) 
+		on delete no action on update no action;
 
 
 
