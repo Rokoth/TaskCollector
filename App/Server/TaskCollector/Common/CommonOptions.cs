2cf51783c73d771281e11b12586b536396f74058
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2021-04-12 09:39:48 +0500

    Common refactoring

--- App/Server/TaskCollector/Common/CommonOptions.cs
+++ App/Server/TaskCollector/Common/CommonOptions.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.Extensions.Options;
+﻿///Copyright 2021 Dmitriy Rokoth
+///Licensed under the Apache License, Version 2.0
+//
+//ref2
 
 namespace TaskCollector.Common
-{    
+{
+    /// <summary>
+    /// Класс хранения настроек
+    /// </summary>
     public class CommonOptions
-    {
+    {        
+        /// <summary>
+        /// Строка полключения к базе данных
+        /// </summary>
         public string ConnectionString { get; set; }        
     }
 }
