5ba13c295293d75e4b9b6ff0c8f15b02331ef28b
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2021-05-30 13:17:22 +0500

    message contract

--- App/Server/TaskCollector/Contract/Model/Message.cs
+++ App/Server/TaskCollector/Contract/Model/Message.cs
@@ -1,17 +1,28 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TaskCollector.Contract.Model
 {
     public class Message : Entity
     {
+		[Display(Name="Уровень ID")]
 		public int Level { get; set; }
+		[Display(Name = "Уровень")]
+		public int LevelTitle { get; set; }
+		[Display(Name = "Заголовок")]
 		public string Title { get; set; }
+		[Display(Name = "Описание")]
 		public string Description { get; set; }
+		[Display(Name = "Обратная связь")]
 		public string FeedbackContact { get; set; }
+		[Display(Name = "Дополнительные поля")]
 		public string AddFileds { get; set; }
+		[Display(Name = "ИД клиента")]
 		public Guid ClientId { get; set; }
+		[Display(Name = "Дата создания")]
 		public DateTimeOffset CreatedDate { get; set; }
-
+		[Display(Name = "Клиент")]
+		public Client Client { get; set; }
 	}
 
 	public class MessageHistory : EntityHistory
