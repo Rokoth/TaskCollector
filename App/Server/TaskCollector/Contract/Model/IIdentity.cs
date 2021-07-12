c89e250077ebaebc8aea281b8f3019772ffe0560
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2021-07-12 08:24:13 +0500

    refactoring

--- App/Server/TaskCollector/Contract/Model/IIdentity.cs
+++ App/Server/TaskCollector/Contract/Model/IIdentity.cs
@@ -1,5 +1,12 @@
-﻿namespace TaskCollector.Contract.Model
+﻿//Copyright 2021 Dmitriy Rokoth
+//Licensed under the Apache License, Version 2.0
+//
+//ref2
+namespace TaskCollector.Contract.Model
 {
+    /// <summary>
+    /// interface of identity classes
+    /// </summary>
     public interface IIdentity
     {
         string Login { get; set; }
