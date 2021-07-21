c0f7144e505ec0198345725142129f5f66e5623f
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2021-07-21 13:41:01 +0500

    Some changes

--- App/Server/TaskCollector/Db/Model/Filter.cs
+++ App/Server/TaskCollector/Db/Model/Filter.cs
@@ -1,14 +1,21 @@
-﻿using System;
+﻿//Copyright 2021 Dmitriy Rokoth
+//Licensed under the Apache License, Version 2.0
+//
+//ref 2
+using System;
 using System.Linq.Expressions;
 
 namespace TaskCollector.Db.Model
 {
+    /// <summary>
+    /// Filter wrapper
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class Filter<T> where T : IEntity
     {        
         public int Page { get; set; }
         public int Size { get; set; }
         public string Sort { get; set; }
-
         public Expression<Func<T, bool>> Selector { get; set; }
     }
 }
