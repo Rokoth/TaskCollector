1de109125304382a5a5a1e014e4dd162384a1833
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2021-08-09 12:15:03 +0500

    refactoring

--- App/Server/TaskCollector/Deploy/IDeployService.cs
+++ App/Server/TaskCollector/Deploy/IDeployService.cs
@@ -1,9 +1,21 @@
-﻿using System.Threading.Tasks;
+﻿//Copyright 2021 Dmitriy Rokoth
+//Licensed under the Apache License, Version 2.0
+//
+//ref2
+using System.Threading.Tasks;
 
 namespace TaskCollector.Deploy
 {
+    /// <summary>
+    /// Deploy Service interface
+    /// </summary>
     public interface IDeployService
     {
+        /// <summary>
+        /// Deploy DB method
+        /// </summary>
+        /// <param name="num">last update</param>
+        /// <returns></returns>
         Task Deploy(int? num = null);
     }
 }
