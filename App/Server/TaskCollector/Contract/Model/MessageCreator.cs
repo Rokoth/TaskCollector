fe1e9f593997434f0c09bb865a7aec4a687bf96b
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2021-08-10 13:00:17 +0500

    techtask, refac

--- App/Server/TaskCollector/Contract/Model/MessageCreator.cs
+++ App/Server/TaskCollector/Contract/Model/MessageCreator.cs
@@ -1,16 +1,56 @@
-﻿using System;
+﻿//Copyright 2021 Dmitriy Rokoth
+//Licensed under the Apache License, Version 2.0
+//
+//ref2
+using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TaskCollector.Contract.Model
 {
-    public class MessageCreator
-    {		
-		public int Level { get; set; }		
+    /// <summary>
+	/// Model for creating message
+	/// </summary>
+	public class MessageCreator
+    {
+		/// <summary>
+		/// Уровень ID
+		/// </summary>
+		[Display(Name = "Уровень ID")]
+		public int Level { get; set; }
+		/// <summary>
+		/// Уровень
+		/// </summary>
+		[Display(Name = "Уровень")]
+		public string LevelTitle { get; set; }
+		/// <summary>
+		/// Заголовок
+		/// </summary>
+		[Display(Name = "Заголовок")]
 		public string Title { get; set; }
+		/// <summary>
+		/// Описание
+		/// </summary>
+		[Display(Name = "Описание")]
 		public string Description { get; set; }
-		public string FeedbackContact { get; set; }		
+		/// <summary>
+		/// Обратная связь
+		/// </summary>
+		[Display(Name = "Обратная связь")]
+		public string FeedbackContact { get; set; }
+		/// <summary>
+		/// Дополнительные поля
+		/// </summary>
+		[Display(Name = "Дополнительные поля")]
+		public string AddFields { get; set; }
+		/// <summary>
+		/// ИД клиента
+		/// </summary>
+		[Display(Name = "ИД клиента")]
 		public Guid ClientId { get; set; }
+		/// <summary>
+		/// Дата создания
+		/// </summary>
+		[Display(Name = "Дата создания")]
 		public DateTimeOffset CreatedDate { get; set; }		
-        public string AddFields { get; set; }
     }
-
 }
